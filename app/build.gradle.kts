31e61668b6c99feea57c9a9f13f4b4b290c7afff
Tanat Arora <tanat.navin@gmail.com>
2024-03-09 19:24:05 +0700

    Add mainActity and mainActivity xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 android {
+    viewBinding {
+        enable = true
+    }
+
     namespace = "com.example.androidfinalpractice"
     compileSdk = 34
 
@@ -49,20 +53,29 @@ android {
     }
 }
 
+val ktor_version = "2.3.4"
+
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    // install ktor for api calls
+    implementation("io.ktor:ktor-client-android:$ktor_version")
+    implementation("io.ktor:ktor-serialization-gson:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
